d89568253ccbf39a566a8108e7e75d452383e860
박세훈 <psh3253@naver.com>
2021-04-04 11:06:47 +0900

    Add delete and all complete

--- src/HW1.java
+++ src/HW1.java
@@ -34,10 +34,10 @@ public class HW1 {
 			Collections.shuffle(keyList, rand);
 			int loopCount = (int)(keyList.size() * 0.95);
 			for (int i = 0; i < loopCount; i++) {
-				//st.delete(keyList.get(i));						// 주석 처리 가능
+				st.delete(keyList.get(i));						// 주석 처리 가능
 			}
-			//System.out.println("\n### 키 삭제 후 트리 정보");			// 주석 처리 가능
-			//print_tree(st);										// 주석 처리 가능. 여기까지 정상적으로 출력되면 100점
+			System.out.println("\n### 키 삭제 후 트리 정보");			// 주석 처리 가능
+			print_tree(st);										// 주석 처리 가능. 여기까지 정상적으로 출력되면 100점
 		} catch (FileNotFoundException e) { e.printStackTrace(); }
 		if (sc != null)
 			sc.close();
@@ -193,7 +193,7 @@ class Tree23<K extends Comparable<K>, V> {
 
 	public void combine(Tree23<K, V> node1, Tree23<K, V> node2) {
 		if (node1.key2 == null) {
-			if (node2.key1.compareTo(node1.key1) < 0) { // o
+			if (node2.key1.compareTo(node1.key1) < 0) {
 				node1.key2 = node1.key1;
 				node1.value2 = node1.value1;
 				node1.right = node1.middle;
@@ -211,7 +211,7 @@ class Tree23<K extends Comparable<K>, V> {
 				node2.middle.parent = node1;
 			}
 		} else {
-			if (node2.key1.compareTo(node1.key1) < 0) { // o
+			if (node2.key1.compareTo(node1.key1) < 0) {
 				Tree23<K, V> parentNode = node1.parent;
 				Tree23<K, V> newNode = new Tree23<>();
 				newNode.key1 = node1.key2;
@@ -305,6 +305,265 @@ class Tree23<K extends Comparable<K>, V> {
 				newNode.root = node1.root;
 				if (parentNode != null)
 					combine(parentNode, node1);
+			}
+		}
+	}
+
+	public void delete(K key) {
+		Tree23<K, V> tempNode = find_node(key);
+		Tree23<K, V> successorNode;
+		if (tempNode.left != null) {
+			if (key.equals(tempNode.key1)) {
+				successorNode = tempNode.middle;
+				while (successorNode.left != null)
+					successorNode = successorNode.left;
+				tempNode.key1 = successorNode.key1;
+				tempNode.value1 = successorNode.value1;
+				successorNode.key1 = null;
+				successorNode.value1 = null;
+			} else {
+				successorNode = tempNode.right;
+				while (successorNode.left != null)
+					successorNode = successorNode.left;
+				tempNode.key2 = successorNode.key1;
+				tempNode.value2 = successorNode.value1;
+				successorNode.key1 = null;
+				successorNode.value1 = null;
+			}
+			deleteProcess(successorNode);
+		} else {
+			if (key.equals(tempNode.key1)) {
+				if (tempNode.key2 != null) {
+					tempNode.key1 = tempNode.key2;
+					tempNode.value1 = tempNode.value2;
+					tempNode.key2 = null;
+					tempNode.value2 = null;
+				} else {
+					tempNode.key1 = null;
+					tempNode.value1 = null;
+					deleteProcess(tempNode);
+				}
+			} else {
+				tempNode.key2 = null;
+				tempNode.value2 = null;
+			}
+		}
+		N--;
+	}
+
+	public void deleteProcess(Tree23<K, V> tempNode) {
+		Tree23<K, V> parentNode = tempNode.parent;
+		if (tempNode.key1 != null) {
+
+		} else if (tempNode.key2 != null) {
+			tempNode.key1 = tempNode.key2;
+			tempNode.value1 = tempNode.value2;
+			tempNode.key2 = null;
+			tempNode.value2 = null;
+		} else {
+			if (tempNode == parentNode.left) {
+				if (parentNode.middle.key2 != null) {
+					tempNode.key1 = parentNode.key1;
+					tempNode.value1 = parentNode.value1;
+					parentNode.key1 = parentNode.middle.key1;
+					parentNode.value1 = parentNode.middle.value1;
+					parentNode.middle.key1 = parentNode.middle.key2;
+					parentNode.middle.value1 = parentNode.middle.value2;
+					parentNode.middle.key2 = null;
+					parentNode.middle.value2 = null;
+					if (tempNode.middle != null)
+						tempNode.left = tempNode.middle;
+
+					if (parentNode.middle.left != null) {
+						tempNode.middle = parentNode.middle.left;
+						tempNode.middle.parent = tempNode;
+					}
+					parentNode.middle.left = parentNode.middle.middle;
+					parentNode.middle.middle = parentNode.middle.right;
+					parentNode.middle.right = null;
+				} else {
+					if (parentNode.key2 != null) {
+						parentNode.middle.key2 = parentNode.middle.key1;
+						parentNode.middle.value2 = parentNode.middle.value1;
+						parentNode.middle.key1 = parentNode.key1;
+						parentNode.middle.value1 = parentNode.value1;
+						parentNode.key1 = parentNode.key2;
+						parentNode.value1 = parentNode.value2;
+						parentNode.key2 = null;
+						parentNode.value2 = null;
+						parentNode.left = parentNode.middle;
+						parentNode.middle = parentNode.right;
+						parentNode.right = null;
+						parentNode.left.right = parentNode.left.middle;
+						parentNode.left.middle = parentNode.left.left;
+						if (tempNode.left != null) {
+							parentNode.left.left = tempNode.left;
+							tempNode.left.parent = parentNode.left;
+						} else if (tempNode.middle != null) {
+							parentNode.left.left = tempNode.middle;
+							tempNode.middle.parent = parentNode.left;
+						} else {
+							parentNode.left.left = null;
+						}
+					} else {
+						parentNode.middle.key2 = parentNode.middle.key1;
+						parentNode.middle.value2 = parentNode.middle.value1;
+						parentNode.middle.key1 = parentNode.key1;
+						parentNode.middle.value1 = parentNode.value1;
+						parentNode.key1 = null;
+						parentNode.value1 = null;
+						parentNode.middle.right = parentNode.middle.middle;
+						parentNode.middle.middle = parentNode.middle.left;
+						parentNode.left = null;
+						if (tempNode.left != null) {
+							parentNode.middle.left = tempNode.left;
+							tempNode.left.parent = parentNode.middle;
+						} else if (tempNode.middle != null) {
+							parentNode.middle.left = tempNode.middle;
+							tempNode.middle.parent = parentNode.middle;
+						} else {
+							parentNode.middle.left = null;
+						}
+						if (parentNode.parent != null) {
+							deleteProcess(parentNode);
+						} else {
+							parentNode.key1 = parentNode.middle.key1;
+							parentNode.value1 = parentNode.middle.value1;
+							parentNode.key2 = parentNode.middle.key2;
+							parentNode.value2 = parentNode.middle.value2;
+							parentNode.right = parentNode.middle.right;
+							parentNode.left = parentNode.middle.left;
+							parentNode.middle = parentNode.middle.middle;
+							if (parentNode.left != null)
+								parentNode.left.parent = parentNode;
+							if (parentNode.middle != null)
+								parentNode.middle.parent = parentNode;
+							if (parentNode.right != null)
+								parentNode.right.parent = parentNode;
+							root = parentNode;
+						}
+					}
+				}
+			} else if(tempNode == parentNode.middle) {
+				if (parentNode.left.key2 != null) {
+					tempNode.key1 = parentNode.key1;
+					tempNode.value1 = parentNode.value1;
+					parentNode.key1 = parentNode.left.key2;
+					parentNode.value1 = parentNode.left.value2;
+					parentNode.left.key2 = null;
+					parentNode.left.value2 = null;
+					if (tempNode.left != null)
+						tempNode.middle = tempNode.left;
+					if (parentNode.left.right != null) {
+						tempNode.left = parentNode.left.right;
+						tempNode.left.parent = tempNode;
+					}
+					parentNode.left.right = null;
+				} else if (parentNode.right != null && parentNode.right.key2 != null) {
+					tempNode.key1 = parentNode.key2;
+					tempNode.value1 = parentNode.value2;
+					parentNode.key2 = parentNode.right.key1;
+					parentNode.value2 = parentNode.right.value1;
+					parentNode.right.key1 = parentNode.right.key2;
+					parentNode.right.value1 = parentNode.right.value2;
+					parentNode.right.key2 = null;
+					parentNode.right.value2 = null;
+					if (tempNode.middle != null)
+						tempNode.left = tempNode.middle;
+					if (parentNode.right.left != null) {
+						tempNode.middle = parentNode.right.left;
+						tempNode.middle.parent = tempNode;
+					}
+					parentNode.right.left = parentNode.right.middle;
+					parentNode.right.middle = parentNode.right.right;
+					parentNode.right.right = null;
+				} else {
+					if (parentNode.key2 != null) {
+						parentNode.left.key2 = parentNode.key1;
+						parentNode.left.value2 = parentNode.value1;
+						parentNode.key1 = parentNode.key2;
+						parentNode.value1 = parentNode.value2;
+						parentNode.key2 = null;
+						parentNode.value2 = null;
+						parentNode.middle = parentNode.right;
+						parentNode.right = null;
+						if (tempNode.left != null) {
+							parentNode.left.right = tempNode.left;
+							tempNode.left.parent = parentNode.left;
+						} else if (tempNode.middle != null) {
+							parentNode.left.right = tempNode.middle;
+							tempNode.middle.parent = parentNode.left;
+						} else {
+							parentNode.left.right = null;
+						}
+					} else {
+						parentNode.left.key2 = parentNode.key1;
+						parentNode.left.value2 = parentNode.value1;
+						parentNode.key1 = null;
+						parentNode.value1 = null;
+						parentNode.middle = null;
+						if (tempNode.left != null) {
+							parentNode.left.right = tempNode.left;
+							tempNode.left.parent = parentNode.left;
+						} else if (tempNode.middle != null) {
+							parentNode.left.right = tempNode.middle;
+							tempNode.middle.parent = parentNode.left;
+						} else {
+							parentNode.left.right = null;
+						}
+						if (parentNode.parent != null) {
+							deleteProcess(parentNode);
+						} else {
+							parentNode.key1 = parentNode.left.key1;
+							parentNode.value1 = parentNode.left.value1;
+							parentNode.key2 = parentNode.left.key2;
+							parentNode.value2 = parentNode.left.value2;
+							parentNode.right = parentNode.left.right;
+							parentNode.middle = parentNode.left.middle;
+							parentNode.left = parentNode.left.left;
+							if (parentNode.left != null)
+								parentNode.left.parent = parentNode;
+							if (parentNode.middle != null)
+								parentNode.middle.parent = parentNode;
+							if (parentNode.right != null)
+								parentNode.right.parent = parentNode;
+							root = parentNode;
+						}
+					}
+				}
+			} else {
+				if (parentNode.middle.key2 != null) {
+					tempNode.key1 = parentNode.key2;
+					tempNode.value1 = parentNode.value2;
+					parentNode.key2 = parentNode.middle.key2;
+					parentNode.value2 = parentNode.middle.value2;
+					parentNode.middle.key2 = null;
+					parentNode.middle.value2 = null;
+					if (tempNode.left != null)
+						tempNode.middle = tempNode.left;
+
+					if (parentNode.middle.right != null) {
+						tempNode.left = parentNode.middle.right;
+						tempNode.left.parent = tempNode;
+					}
+
+					parentNode.middle.right = null;
+				} else {
+					parentNode.middle.key2 = parentNode.key2;
+					parentNode.middle.value2 = parentNode.value2;
+					parentNode.key2 = null;
+					parentNode.value2 = null;
+					parentNode.right = null;
+					if (tempNode.left != null) {
+						parentNode.middle.right = tempNode.left;
+						tempNode.left.parent = parentNode.middle;
+					} else if (tempNode.middle != null) {
+						parentNode.middle.right = tempNode.middle;
+						tempNode.middle.parent = parentNode.middle;
+					} else {
+						parentNode.middle.right = null;
+					}
+				}
 			}
 		}
 	}
